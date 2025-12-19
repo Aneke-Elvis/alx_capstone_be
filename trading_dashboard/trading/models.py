5b9e1e3039aecb6b9a8e93ffedab1b3069cf571e
Aneke-Elvis <anekeelvis23@gmail.com>
2025-12-19 20:54:25 +0100

    Fix Asset __str__ typo causing trades endpoint crash

--- trading_dashboard/trading/models.py
+++ trading_dashboard/trading/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Asset (models.Model):
     symbol = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
     asset_type = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.symobol
+        return self.symbol
 
 
 class Portfolio(models.Model):
@@ -24,4 +25,5 @@ class Trade(models.Model):
     entry_price = models.FloatField()
     exit_price = models.FloatField(null=True, blank=True)
     quantity = models.FloatField()
+
     created_at = models.DateTimeField(auto_now_add=True)
