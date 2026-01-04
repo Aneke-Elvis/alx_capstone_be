5ba75915b4d25fd5a1b469bbb8167ef44761476d
Aneke-Elvis <anekeelvis23@gmail.com>
2026-01-04 16:43:19 +0100

    Update backend README and minor backend configuration

--- trading_dashboard/trading/models.py
+++ trading_dashboard/trading/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Asset (models.Model):
-    symbol = models.CharField(max_length=20)
+    symbol = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
     asset_type = models.CharField(max_length=50)
 
@@ -19,9 +19,18 @@ class Portfolio(models.Model):
         return f"{self.user.username} Portfolio"
     
 class Trade(models.Model):
+    BUY = "BUY"
+    SELL = "SELL"
+
+    TRADE_TYPE_CHOICES = [
+        (BUY, "Buy"),
+        (SELL, "Sell"),
+    ]
     portfolio = models.ForeignKey(Portfolio,on_delete=models.CASCADE )
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE )
-    trade_type = models.CharField(max_length=10)
+    trade_type = models.CharField(
+        max_length=10, 
+        choices=TRADE_TYPE_CHOICES )
     entry_price = models.FloatField()
     exit_price = models.FloatField(null=True, blank=True)
     quantity = models.FloatField()
